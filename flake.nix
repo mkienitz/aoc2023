12cd965356dd63ca83fe182401c58c60d313047e
Maximilian Kienitz <max@kienitz.dev>
2023-11-30 21:30:00 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Advent Of Code 2023";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      haskellPackages = pkgs.haskell.packages."ghc947";
+    in {
+      formatter = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        buildInputs =
+          [
+            (haskellPackages.ghcWithPackages (hpkgs:
+              with hpkgs; [
+                # Tools
+                ghcid
+                haskell-language-server
+                hlint
+                hoogle
+                ormolu
+                # Dependencies
+                megaparsec
+                split
+              ]))
+          ]
+          ++ (with pkgs; [
+            nil
+          ]);
+      };
+    });
+}

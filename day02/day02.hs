28d3aca66749a747f77a97e6060ba9626b46d3a5
Maximilian Kienitz <max@kienitz.dev>
2023-12-03 02:03:02 +0100

    feat(day02): refactor

--- day02/day02.hs
+++ day02/day02.hs
@@ -1,13 +1,17 @@
 module Main (main) where
 
 import Data.Either (fromRight)
+import Data.Function (on)
+import Data.List (groupBy)
+import Data.List.Extra (maximum, sortOn)
 import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
 
 -- Types :) --
 data Color = Red | Green | Blue
-  deriving (Show, Eq)
+  deriving (Eq, Show, Ord)
 
 type Draw = (Int, Color)
 
@@ -17,18 +21,9 @@ type Game = (Int, [Round])
 
 type Task = [Game]
 
-newtype Total = Total (Int, Int, Int)
-  deriving (Eq)
-
-instance Ord Total where
-  (<=) (Total (r1, g1, b1)) (Total (r2, g2, b2)) = r1 <= r2 && g1 <= g2 && b1 <= b2
-
 type Parser = Parsec Void String
 
 -- Parser :) --
-intP :: Parser Int
-intP = read <$> some numberChar
-
 colorP :: Parser Color
 colorP =
   choice
@@ -38,13 +33,16 @@ colorP =
     ]
 
 drawP :: Parser Draw
-drawP = (,) <$> intP <* char ' ' <*> colorP
+drawP = (,) <$> decimal <* char ' ' <*> colorP
 
 roundP :: Parser Round
 roundP = some (drawP <* optional (string ", "))
 
 gameP :: Parser Game
-gameP = (,) <$> (string "Game " *> intP <* string ": ") <*> some (roundP <* optional (string "; "))
+gameP =
+  (,)
+    <$> (string "Game " *> decimal <* string ": ")
+    <*> some (roundP <* optional (string "; "))
 
 taskP :: Parser Task
 taskP = some (gameP <* optional newline) <* eof
@@ -53,22 +51,25 @@ parseInput :: String -> Task
 parseInput = fromRight [] . runParser taskP "input.txt"
 
 -- Logic :) --
-calcPerColor :: ([Int] -> a) -> [Draw] -> (a, a, a)
-calcPerColor f draws = (apply Red, apply Green, apply Blue)
-  where
-    apply col = f . map fst . filter ((==) col . snd) $ draws
-
 p1 :: Task -> Int
-p1 = sum . map fst . filter (isValidGame (Total (12, 13, 14)))
+p1 = sum . map fst . filter (all (all isValidDraw) . snd)
   where
-    isValidGame total = all (isValidRound total) . snd
-    isValidRound total = (<= total) . Total . calcPerColor sum
+    isValidDraw (n, col) = case col of
+      Red -> n <= 12
+      Green -> n <= 13
+      Blue -> n <= 14
 
 p2 :: Task -> Int
-p2 = sum . map toPower
-  where
-    toPower :: Game -> Int
-    toPower = (\(r, g, b) -> r * g * b) . calcPerColor maximum . concat . snd
+p2 =
+  sum
+    . map
+      ( product -- get power of cube set
+          . map (maximum . map fst) -- get maximum count per color
+          . groupBy ((==) `on` snd) -- group by color
+          . sortOn snd -- make draws adjacent by color
+          . concat -- join rounds
+          . snd -- get rounds
+      )
 
 main :: IO ()
 main = readFile "input.txt" >>= print . sequence [p1, p2] . parseInput
